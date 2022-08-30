3ac36adf4484ecd06fb6ea3e2b128c8610d69af7
na495 <66860637+na495@users.noreply.github.com>
2022-08-30 11:53:55 +0900

    random

--- PYTHON/NKO-BtnMacro.py
+++ PYTHON/NKO-BtnMacro.py
@@ -13,24 +13,23 @@ driver_time = webdriver.Chrome(
     '/Users/nayeon/Documents/VScode_C/WHAT-/PYTHON/chromedriver')  # 서버 시간용 웹드라이버 입력
 
 # 위버스 페이지 열기
-driver.get('https://www.weverse.io/seventeen/notice/2962')
-# 네이비즘 페이지 열기
+driver.get('https://fanevent.weverse.io/apply?eventId=580&hl=ko')
+# 시계 페이지 열기
 driver_time.get("http://nko.kr/?url=https%3A%2F%2Fwww.weverse.io%2F")
 
 
 severtime = driver_time.find_element(
     By.ID, 'nkoclockdisplay')
 
-submit = driver.find_element(By.CLASS_NAME, 'bVoNaW')  # 제출 버튼
-
 while True:
-    a = severtime.text  # 네이비즘 시-분-초 가져오기
+    a = severtime.text  # 시계 사이트의 시-분-초 가져오기
     time = re.findall("[0-9]+", a)  # 가져온 시-분-초를 배열하기
 
-    if(time[1] == '50' and time[2] == '59'):  # time[4] : 분, time[5] : 초
-        if(int(time[3]) >= 800):  # msec[0] : 밀리초
+    if(time[1] == '59' and time[2] == '59'):  # time[4] : 분, time[5] : 초
+        if(int(time[3]) >= 650):  # msec[0] : 밀리초
+            submit = driver.find_element(By.CLASS_NAME, 'dbmIBg')  # 제출 버튼
             submit.click()
 
             a = severtime.text
-            print(a)  # 네이비즘 시-분-초 출력
+            print(a)  # 시계 사이트 시-분-초 출력
             break

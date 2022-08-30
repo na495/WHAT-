0c4184ec481dc692ac3a6b8c878456b531ce26df
na495 <66860637+na495@users.noreply.github.com>
2022-08-30 16:29:06 +0900

    update

--- RANDOM/RANDOM.js
+++ RANDOM/RANDOM.js
@@ -1,259 +1,468 @@
 var Engilsh = new Array(
     // 1
-    '나는 네게 익숙해',
+    "PASS",
+    "나는 네게 익숙해",
+    "I'm used to you",
     '나는 그 소음에 익숙해',
+    "I'm used to the noise",
     '나는 이 추위에 익숙해',
+    "I'm used to the cold",
     '나는 이 억양에 익숙해',
+    "I'm used to the accent",
     '나는 정장을 입는 것에 익숙해',
+    "I'm used to wearing a suit",
     '나는 여행하는 것에 익숙해',
+    "I'm used to traveling",
     '나는 책을 읽는 것에 익숙해',
+    "I'm used to reading",
     '나는 커피를 마시는 것에 익숙해',
+    "I'm used to drinking coffee",
     // 2
     '유감스럽지만 나는 너를 도와줄 수 없어',
+    "I'm afraid i can't help you",
     '유감스럽지만 나 시간이 하나도 없어',
+    "I'm afraid i don't have any time",
     '유감스럽지만 음식이 너무 차가워요',
+    "I'm afraid the food is too cold",
     '유감스럽지만 밖에 비가 와요',
+    "I'm afraid it's rainning outside",
     '유감스럽지만 밖에 눈이 와요',
+    "I'm afraid it's snowing outside",
     '유감스럽지만 스테이크가 너무 차가워요',
-    '네가 그것을 할 수 있다고 확신해',
+    "I'm afraid the steak is too cold",
     // 3
+    '네가 그것을 할 수 있다고 확신해',
+    "I'm sure you can do it",
     '네가 잘했을 거라고 확신해',
+    "I'm sure you did a good job",
     '그가 너를 좋아한다고 확신해',
+    "I'm sure he likes you",
     '네가 이걸 좋아할 것이라고 확신해',
+    "I'm sure you will like it",
     '네가 이길거라고 확신해',
+    "I'm sure you will win",
     '그녀가 늦을 거라고 확신해',
+    "I'm sure she will be late",
     // 4
     '나는 내가 할 수 있을지 잘 모르겠어',
+    "I'm not sure i can do this",
     '내가 이걸 이해할 수 있는지 잘 모르겠어',
+    "I'm not sure i can understand this",
     '그게 가능한지 잘 모르겠어',
+    "I'm not sure it's possible",
     '그게 안전한지 잘 모르겠어',
+    "I'm not sure it's safe",
     '그게 충분한지 잘 모르겠어',
+    "I'm not sure it's enough",
     '우리가 이곳에 들어갈 수 있는지 잘 모르겠어',
+    "I'm not sure we can go in here",
     // 5
     '너 계획을 갖고 있는거 확실해?',
+    "Are you sure you have a plan?",
     '너 이사하고 싶은거 확실해?',
+    "Are you sure you want to move?",
     '너 그만두고 싶은거 확실해?',
+    "Are you sure you want to quit",
     '너 티켓 갖고 있는거 확실해?',
+    "Are you sure you have a ticket",
     '너 마칠 수 있는거 확실해?',
+    "Are you sure you can finish it?",
     '너 목표를 갖고 있는게 확실해?',
+    "Are you sure you have a goal?",
     // 6
     '미안하다고 말하는 거야',
+    "I'm saying i'm sorry",
     '나는 이해할 수 없다고 말하는 거야',
+    "I'm saying i don't understand",
     '그건 내 꿈이라고 말하는 거야',
+    "I'm saying it's my dream",
     '나는 걱정된다고 말하는 거야',
+    "I'm saying i'm worried",
     // 7
     '네가 떠나야 한다고 말하는 게 아니야',
+    "I'm not saying you should leave",
     '내가 항상 옳다고 말하는 게 아니야',
+    "I'm not saying i'm always right",
     '그게 쉬울거라고 말하는 게 아니야',
+    "I'm not saying it'll be easy",
     '네가 가야한다고 말하는 게 아니야',
+    "I'm not saying you should go",
     '그게 어려울 거라고 말하는 게 아니야',
+    "I'm not saying it'll be difficult",
     // 8
     '나는 너에 대해 이야기하고 있어',
+    "I'm talking about you",
     '나는 스포츠에 대해 이야기하고 있어',
+    "I'm talking about sport",
     '내 노트북에 대해 이야기하고 있어',
+    "I'm talking about my laptop",
     '서울에서 그녀를 만나는 것에 대해 이야기하고 있어',
+    "I'm talking about meeting her in seoul",
     '여행하는 것에 대해 이야기하고 있어',
+    "I'm talking about traveling",
     '영어를 공부하는 것 대해 이야기하고 있어',
+    "I'm talking about studying Engilsh",
     // 9
     '너 우리에 대해 얘기하는 거야?',
+    "Are you talking about us?",
     '너 그 영화에 대해 얘기하는 거야?',
+    "Are you talking about the movie?",
     '내 남자친구에 대해 얘기하는 거야?',
+    "Are you talking about my boyfriend?",
     '너 그 노래에 대해 얘기하는 거야?',
+    "Are you talking about the song?",
     // 10
     '너 그곳에 머무를 계획이야?',
+    "Are you planning to stey there?",
     '너 여행할 계획이야?',
+    "Are you planning to travel?",
     '너 외식할 계획이야?',
+    "Are you planning to eat out?",
     '이것을 바꿀 계획이야?',
+    "Are you planning to change this?",
     '저것을 바꿀 계획이야?',
+    "Are you planning to chang that?",
     // 11
     '나는 더 이상 어린아이가 아니었어',
+    "I was no longer a child",
     '나 여기서 더 이상 일하고 있지 않았어',
+    "I was no longer working here",
     '나는 더 이상 학생이 아니었어',
+    "I was no longer a student",
     '나는 더 이상 혼자가 아니었어',
+    "I was no longer alone",
     // 12
     '나는 떠나달라고 요청을 받았어',
+    "I was asked to leave",
     '나 도와달라고 요청을 받았어',
+    "I was asked to help out",
     '나는 너에게 연락해달라는 요청을 받았어',
+    "I was asked to contact you",
     '나는 그에게 연락해달라는 요청을 받았어',
+    "I was asked to contact him",
     '나는 여기서 기다리라고 요청을 받았어',
+    "I was asked to weit here",
     '나 커피를 만들어달라는 요청을 받았어',
+    "I was asked to make coffee",
     '나는 일찍 와달라는 요청을 받았어',
+    "I was asked to come early",
     // 13
     '나는 여기 있기로 되어 있었어',
+    "I was supposed to be here",
     '나 내 친구를 만나기로 되어 있었어',
+    "I was supposed to meet my friend",
     '나 오늘 집에 가기로 되어 있었어',
+    "I was supposed to go home today",
     '나 그걸 갖고 있기로 되어 있었어',
+    "I was supposed to have it",
     '나는 그걸 했었어야 했어',
+    "I was supposed to do it",
     '나 오늘 학교에 가기로 되어 있었어',
+    "I was supposed to go to school today",
     '나 여기에 내려야 했었어',
+    "I was supposed to get off here",
     '나 여기에 주차했어야 했었어',
+    "I was supposed to park here",
     '나 내 친구에게 연락했어야 했었어',
+    "I was supposed to contact my friend",
     // 14
     '나는 이것을 보면 안되는 거였어',
+    "I wasn't supposed to see this",
     '나는 그것에 대해 알면 안되는 거였어',
+    "I wasn't supposed to khow about it",
     '나는 이것을 말하면 안되는 거였어',
+    "I wasn't supposed to say this",
     '나는 그것에 대해 물으면 안되는 거였어',
+    "I wasn't supposed to ask about it",
     '나는 너무 많이 먹으면 안되는 거였어',
+    "I wasn't supposed to eat so much",
     '나는 집에 있으면 안되는 거였어',
-    '나는 걱정하면 안되는 거였어',
+    "I wasn't supposed to be at home",
+    '나는 그것에 대해 걱정하면 안되는 거였어',
+    "I wasn't supposed to worry about it",
     // 15
     '나 네게 말할까 생각했어',
+    "I was thinking about telling you",
     '나 돌아갈까 생각했어',
+    "I was thinking about going back",
     '나 외출할까 생각했어',
+    "I was thinking about going out",
     '나는 이사 갈까 생각했어',
+    "I was thinking about moveing away",
     '나는 다이어트를 할까 생각했어',
+    "I was thinking about going on a diet",
     '나는 새 휴대폰을 살까 생각했어',
+    "I was thinking about buying a new phone",
     '나는 사업을 시작할까 생각했어',
+    "I was thinking about strating a business",
     // 16
     '내가 너라면 좋겠어',
+    "I wish I were you",
     '내가 뉴욕에 있다면 좋겠어',
+    "I wish I were in newyork",
     '내가 서울에 있다면 좋겠어',
-    '내가 새 (한마리)라면 좋겠어 ',
+    "I wish I were in seoul",
+    '내가 새 (한마리)라면 좋겠어',
+    "I wish I were a bird",
     '너와 함께라면 좋겠어 ',
+    "I wish I were with you",
     '내 친구들과 함께라면 좋겠어',
+    "I wish I were with my friends",
     // 17
     '기억하는 것은 중요해',
+    "It's important to remember",
     '용서하는 것은 중요해',
+    "It's important to forgive",
     '질문을 하는 것은 중요해',
+    "It's important to ask questions",
+    '연습 하는 것은 중요해',
+    "It's important to practice",
     '운동을 계속 하는 것은 중요해',
+    "It's important to keep exercising",
     '주의를 기울이는 것은 중요해',
+    "It's important to pay attention",
     // 18
-    '네가 (어떻게) 느끼는지가 중요해',
+    '네가 어떻게 느끼는지가 중요해',
+    "It's about how you feel",
     '네가 얼마나 잘하는 지가 중요해',
+    "It's about how good you are",
     '네가 어떻게 반응하는지가 중요해',
+    "It's about how you react",
     '네가 얼마나 힘이 센지에 달려있어',
+    "It's about how much strong you are",
     '네가 얼마나 많이 노력하는지가 중요해',
+    "It's about how much you try",
     '네가 얼마나 많이 연습하는지가 중요해',
+    "It's about how much you practice",
     // 19
     '먹기엔 너무 늦었어',
+    "It was too late to eat",
     '운동하기엔 너무 늦었어',
+    "It was too late to work out",
     '돌아가기엔 너무 늦었어',
+    "It was too late to go back",
     '그에게 문자를 하기엔 너무 늦었어',
+    "It was too late to text him",
     '환불을 받기엔 너무 늦었어',
+    "It was too late to get a refund",
     '커피를 마시기엔 너무 늦었어',
+    "It was too late to drink coffee",
     // 20
     '이길 가능성이 있어',
+    "It's possible to win",
     '그것을 고칠 가능성이 있어',
+    "It's possible to fix it",
     '그것을 작동하지 않을 가능성이 있어',
+    "It's possible it won't work",
     '네가 모를 가능성이 있어',
+    "It's possible you don't know",
     '계속될 가능성이 있어',
+    "It's possible to go on",
     // 21
     '그것은 너 때문이야',
+    "That's because of you",
     '그것은 내가 너를 사랑하기 때문이야',
+    "That's because i love you",
     '그것은 내가 그를 사랑하기 때문이야',
+    "That's because i love him",
     '그것은 내가 잊어버렸기 때문이야',
+    "That's because i forgot",
     '이것은 복잡하기 때문이야',
+    "That's because complicated",
     '우리는 친구 사이이기 때문이야',
+    "That's because we are friends",
     // 22
     '이번이 처음이야',
+    "this is the first time",
     '내가 여기 온 게 이번이 처음이야',
-    '이것이 처음 네가 해야 하는 일이야',
+    "this is the first time i've been here",
+    '이것이 네가 해야 하는 첫번째 일이야',
+    "this is the first thing you should do",
+    '이것이 내가 해야 하는 첫번째 일이야',
+    "this is the first thing i should do",
     '이것이 내가 산 첫 번째 차야',
+    "this is the first car i bought",
     // 23
     '이것은 그 이유들 중에 하나야',
+    "this is one of the reasons",
     '이것은 그 문제들 중에 하나야',
+    "this is one of the problems",
     '이것은 내가 좋아하는 노래들 중에 하나야',
+    "this is one of my favorite songs",
     '이것은 그 최고의 식사 중에 하나야',
+    "this is one of the best meals",
     // 24
     '문제가 (하나)있었어',
+    "there's been a problem",
     '실수가 (하나)있었어',
+    "there's been a mistake",
     '사고가 (하나)있었어',
+    "there's been an accident",
     '폭팔이 있었어',
+    "there's been an explosion",
     // 25
     '걱정할 필요 없어',
+    "There's no need to worry",
     '언쟁할 필요 없어',
+    "There's no need to argue",
     '대답할 필요 없어',
+    "There's no need to respond",
     '미안해할 필요 없어',
-    // 26
-    '울 이유가 없어',
-    '포기할 이유가 없어',
-    '위험을 부담할 이유가 없어',
-    '시도(하는 것)를 멈출 이유가 없어',
-    //27
-    '그것을 멈출 방법이 없었어',
-    '그것을 예방할 방법이 없었어',
-    '알아낼 방법이 없었어',
-    '그를 구할 방법이 없었어',
-    // 28
-    '내가 좋아하는 게 있어',
-    '너에게 보여주고 싶은 뭔가가 있어',
-    '네가 좋아할지도 모르는 뭔가가 있어',
-    '네가 알아야하는 게 있어',
-    '내가 물어보고 싶은 게 있어',
-    // 29
-    '나 그 뉴스 봤어',
-    '나 많은 변화들을 봤어',
-    '나 이전에 그녀를 본 적이 있어',
-    '나 너 우는 거 봤어',
-    '나 그거 다 봤어',
-    '나 이 영화 봤어',
-    // 30
-    '나 그 영화 아직 못봤어',
-    '나 그 메모 아직 못 봤어',
-    '나 한동안 너를 못 봤어',
-    '나 특별한 어떤 것도 못 봤어',
-    '나 그를 오랫동안 못봤어',
-    // 31
-    '나 배우러 왔어',
-    '나 도와주러 왔어',
-    '나 방문하러 왔어',
-    '나 너를 보러왔어',
-    // 32
-    '나 달라지기로 결정했어',
-    '나 돌아가기로 결정했어',
-    '나 넘어가기로 결정했어',
-    '나 받아들이기로 결정했어',
-    //33
-    '너 오는 게 좋을거야',
-    '너 조심하는 게 좋을거야',
-    '너 멈추는 게 좋을거야',
-    '너 일찍 떠나는 게 좋을거야',
-    '너 서두르는 게 좋을거야',
-    // 34
-    '너 여기 있지 않는 게 좋을거야',
-    '너 울지 않는게 좋을거야',
-    '너  불평하지 않는게 좋을거야',
-    '너 잠들어버리지 않는게 좋을거야',
-    '너 알지 않는게 좋을거야',
-    // 35
-    '나 내가 무엇을 하고 있었는지 전혀 몰랐어',
-    '나 네가 신경쓰는지 전혀 몰랐어',
-    '너 네가 아팠는지 전혀 몰랐어',
-    '나 그게 어디였는지 전혀 몰랐어',
-    '나 그게 너였는지 전혀 몰랐어',
-    // 36
-    '너와는 상관없어',
-    '내 직업과는 상관없어',
-    '네가 봤던 것과 상관없어',
-    '내가 너에게 말해준 것과는 상관없어',
-    '일어난 일과는 상관없어',
-    // 37
-    '네가 올 수 있는지 알려줘',
-    '내가 무엇을 할 수 있는지 알려줘',
-    '네가 어디에 있는지 알려줘',
-    '네가 흥미가 있다면 알려줘',
-    '어떤 상황인지 알려줘',
-    '어떻게 돼가는지 알려줘',
-    // 38
-    '지도를 확인헤 볼게',
-    '내가 무엇을 할 수 있는지를 확인해볼게',
-    '내가 할 수 있지를 확인해 볼게',
-    '그게 효과가 있는지 확인해 볼게',
-    '내가 도울 수 있는지 확인해 볼게',
-    // 39
-    '너에게 (하나의)이야기를 말해줄게',
-    '너에게 비밀을 말해줄게',
-    '너에게 나의 관해서 말해줄게',
-    '너에게 무엇이 일어났는지 알려줄게',
-    '너에게 진실을 말해줄게',
-    // 40
-    '너에게 그 결과를 알려줄게',
-    '너에게 가능한 한 빨리 알려줄게',
-    '너에게 지금 당장 알려줄게',
-    '너에게 네가 필요하면 알려줄게',
+    "There's no need to be sorry",
+    // // 26
+    // '울 이유가 없어',
+    // '포기할 이유가 없어',
+    // '위험을 부담할 이유가 없어',
+    // '시도(하는 것)를 멈출 이유가 없어',
+    // //27
+    // '그것을 멈출 방법이 없었어',
+    // '그것을 예방할 방법이 없었어',
+    // '알아낼 방법이 없었어',
+    // '그를 구할 방법이 없었어',
+    // // 28
+    // '내가 좋아하는 게 있어',
+    // '너에게 보여주고 싶은 뭔가가 있어',
+    // '네가 좋아할지도 모르는 뭔가가 있어',
+    // '네가 알아야하는 게 있어',
+    // '내가 물어보고 싶은 게 있어',
+    // // 29
+    // '나 그 뉴스 봤어',
+    // '나 많은 변화들을 봤어',
+    // '나 이전에 그녀를 본 적이 있어',
+    // '나 너 우는 거 봤어',
+    // '나 그거 다 봤어',
+    // '나 이 영화 봤어',
+    // // 30
+    // '나 그 영화 아직 못봤어',
+    // '나 그 메모 아직 못 봤어',
+    // '나 한동안 너를 못 봤어',
+    // '나 특별한 어떤 것도 못 봤어',
+    // '나 그를 오랫동안 못봤어',
+    // // 31
+    // '나 배우러 왔어',
+    // '나 도와주러 왔어',
+    // '나 방문하러 왔어',
+    // '나 너를 보러왔어',
+    // // 32
+    // '나 달라지기로 결정했어',
+    // '나 돌아가기로 결정했어',
+    // '나 넘어가기로 결정했어',
+    // '나 받아들이기로 결정했어',
+    // //33
+    // '너 오는 게 좋을거야',
+    // '너 조심하는 게 좋을거야',
+    // '너 멈추는 게 좋을거야',
+    // '너 일찍 떠나는 게 좋을거야',
+    // '너 서두르는 게 좋을거야',
+    // // 34
+    // '너 여기 있지 않는 게 좋을거야',
+    // '너 울지 않는게 좋을거야',
+    // '너  불평하지 않는게 좋을거야',
+    // '너 잠들어버리지 않는게 좋을거야',
+    // '너 알지 않는게 좋을거야',
+    // // 35
+    // '나 내가 무엇을 하고 있었는지 전혀 몰랐어',
+    // '나 네가 신경쓰는지 전혀 몰랐어',
+    // '너 네가 아팠는지 전혀 몰랐어',
+    // '나 그게 어디였는지 전혀 몰랐어',
+    // '나 그게 너였는지 전혀 몰랐어',
+    // // 36
+    // '너와는 상관없어',
+    // '내 직업과는 상관없어',
+    // '네가 봤던 것과 상관없어',
+    // '내가 너에게 말해준 것과는 상관없어',
+    // '일어난 일과는 상관없어',
+    // // 37
+    // '네가 올 수 있는지 알려줘',
+    // '내가 무엇을 할 수 있는지 알려줘',
+    // '네가 어디에 있는지 알려줘',
+    // '네가 흥미가 있다면 알려줘',
+    // '어떤 상황인지 알려줘',
+    // '어떻게 돼가는지 알려줘',
+    // // 38
+    // '지도를 확인헤 볼게',
+    // '내가 무엇을 할 수 있는지를 확인해볼게',
+    // '내가 할 수 있지를 확인해 볼게',
+    // '그게 효과가 있는지 확인해 볼게',
+    // '내가 도울 수 있는지 확인해 볼게',
+    // // 39
+    // '너에게 (하나의)이야기를 말해줄게',
+    // '너에게 비밀을 말해줄게',
+    // '너에게 나의 관해서 말해줄게',
+    // '너에게 무엇이 일어났는지 알려줄게',
+    // '너에게 진실을 말해줄게',
+    // // 40
+    // '너에게 그 결과를 알려줄게',
+    // '너에게 가능한 한 빨리 알려줄게',
+    // '너에게 지금 당장 알려줄게',
+    // '너에게 네가 필요하면 알려줄게',
 )
 
+var sum = 0;
+
+function next() {
+    onoff = 0;
+    document.getElementById('answer').style.color = 'black';
+    document.getElementById('done').style.visibility = "hidden";
+    document.getElementById('text2').style.visibility = "hidden";
+    document.getElementById('answer').value = '';
+    cnt();
+}
+
 function BtnClick() {
+    next();
+
     var pick = Math.floor(Math.random() * Engilsh.length);
+    if (pick % 2 == 0) {
+        pick += 1;
+    }
+    console.log(pick);
     document.querySelector('#text').innerHTML = Engilsh[pick];
+    document.querySelector('#text2').innerHTML = Engilsh[pick + 1];
+}
+
+function submit() {
+    var answer = document.getElementById('answer').value;
+    var text2 = document.getElementById('text2').innerHTML;
+
+    if (answer == text2) {
+        document.getElementById('answer').style.color = 'blue';
+        document.getElementById('done').style.visibility = "visible";
+
+    } else {
+        document.getElementById('answer').style.color = 'red';
+        addlist()
+    };
+}
+
+function see() {
+    let text2 = document.getElementById('text2').innerHTML;
+    document.getElementById('text2').style.visibility = "visible";
+}
+
+function addlist() {
+    let text = document.getElementById('text').innerHTML;
+    let text2 = document.getElementById('text2').innerHTML;
+    let answer = document.getElementById('answer').value;
+
+    const li = document.createElement('li');
+    li.innerHTML = (text + '  /  ' + text2 + '  /  ' + answer);
+    document.getElementById('list').appendChild(li);
+}
+
+function removeList() {
+    let List = document.getElementById('list');
+    while (List.hasChildNodes()) {
+        List.removeChild(List.firstChild);
+    }
+    document.getElementById('list').style.visibility = "hidden";
+}
+
+function cnt() {
+    sum += 1;
+    document.getElementById("cnt").innerHTML = sum;
+}
+
+function listview() {
+    document.getElementById('list').style.visibility = "visible";
 }
